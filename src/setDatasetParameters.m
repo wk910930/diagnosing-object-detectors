e0efa58984d9caae76fc06769c60797fc639df73
Kun Wang <wk910930@gmail.com>
2017-04-30 10:03:57 +0800

    Auto formatting
    * Ctrl + A, Ctrl + I

--- src/setDatasetParameters.m
+++ src/setDatasetParameters.m
@@ -3,7 +3,7 @@ function dataset_params = setDatasetParameters(dataset)
 %
 % Sets machine-specific and datset-specific parameters such as image paths.
 %
-% Required parameters: 
+% Required parameters:
 %   imdir: directory containing images
 %   objnames_all{nclasses}: names for each object class, order specifies
 %     index for each class
@@ -13,8 +13,8 @@ function dataset_params = setDatasetParameters(dataset)
 %     classes in an equivalence set is considered similar (symmetric binary
 %     confusion matrix can be encoded as a set of pairs); sets consist of
 %     indices into classes given by objnames_all
-%  summary_sets{nsets}: sets of indices that will be used to summarize 
-%     stastics 
+%  summary_sets{nsets}: sets of indices that will be used to summarize
+%     stastics
 %  summary_setnames{nsets}: names of each set (e.g., animal)
 
 switch lower(dataset)
@@ -28,29 +28,28 @@ switch lower(dataset)
     dataset_params.objnames_extra = {'aeroplane', 'bicycle', 'bird', 'boat', 'cat', ...
       'chair', 'diningtable'}; % required parameter: specify objects with extra annotation -- set to empty set if not using VOC2007
     dataset_params.confidence_threshold = -Inf; % minimum confidence to be included in analysis (e.g., set to 0.01 to improve speed)
-
+    
     % all object names
     dataset_params.objnames_all = {'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', ...
-       'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', ...
-       'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'}; 
+      'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', ...
+      'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'};
     
     % specify sets of similar objects
     animals = [3 8 10 12 13 15 17];    % animals + person (15)
     vehicles1 = [1 4 6 7 19 2 14]; % all vehicles (may want to exclude bicycle motorcycle)
     vehicles2 = [2 14]; % bicycle motorcycle
-    furniture = [9 11 18]; % chair, table, sofa    
+    furniture = [9 11 18]; % chair, table, sofa
     airobjects = [1 3]; % bird, airplane
-    dataset_params.similar_classes = {animals, vehicles1, vehicles2, furniture, airobjects}; 
-
+    dataset_params.similar_classes = {animals, vehicles1, vehicles2, furniture, airobjects};
+    
     % specify summary sets
-    dataset_params.summary_sets = cat(2, {[3 8 10 12 13 17], [1 4 6 7 19 2 14], [9 11 18]});        
-    dataset_params.summary_setnames = {'animals', 'vehicles', 'furniture'};    
+    dataset_params.summary_sets = cat(2, {[3 8 10 12 13 17], [1 4 6 7 19 2 14], [9 11 18]});
+    dataset_params.summary_setnames = {'animals', 'vehicles', 'furniture'};
     
     % localization criteria
     dataset_params.iuthresh_weak = 0.1;  % intersection/union threshold
-    dataset_params.idthresh_weak = 0;    % intersection/det_area threshold   
+    dataset_params.idthresh_weak = 0;    % intersection/det_area threshold
     dataset_params.iuthresh_strong = 0.5;  % intersection/union threshold
-    dataset_params.idthresh_strong = 0;    % intersection/det_area threshold 
+    dataset_params.idthresh_strong = 0;    % intersection/det_area threshold
     
 end
-
--- src/setDetectorInfo.m
+++ src/setDetectorInfo.m
@@ -7,42 +7,42 @@ switch detector
   case 'felzenszwalb_v2'
     detpath = '../detections/felzenszwalb_v2/fel_v2_VOC2007_%s_det.txt';
     resultdir = '../results/felzenszwalb_v2';
-    detname = 'FGMR (v2)';  
+    detname = 'FGMR (v2)';
   case 'felzenszwalb_v3'
     detpath = '../detections/felzenszwalb_v3/fel_v3_VOC2007_%s_det.txt';
     resultdir = '../results/felzenszwalb_v3';
-    detname = 'FGMR (v3)';  
+    detname = 'FGMR (v3)';
   case 'felzenszwalb_v4'
     detpath = '../detections/felzenszwalb_v4/fel_v4_VOC2007_%s_det.txt';
-    resultdir = '../results/felzenszwalb_v4';  
-    detname = 'FGMR (v4)';    
+    resultdir = '../results/felzenszwalb_v4';
+    detname = 'FGMR (v4)';
   case 'vedaldi2009'
     detpath = '../detections/vedaldi2009/ts07full-%s-st0.txt';
     resultdir = '../results/vedaldi2009';
-    detname = 'VGVZ 2009';  
+    detname = 'VGVZ 2009';
   case 'cnn_v7-bb'
     detpath = '../detections/cnn_v7_bb/comp4_det_test_%s.txt';
-    resultdir = '../results/cnn_v7_bb';  
+    resultdir = '../results/cnn_v7_bb';
     detname = 'CNN v7 bb';
   case 'candidate_detector1'
     detpath = '../detections/candidate_detector1/comp4_det_test_%s.txt';
-    resultdir = '../results/candidate_detector1';  
+    resultdir = '../results/candidate_detector1';
     detname = 'condidate_detector1';
   case 'candidate_detector2'
     detpath = '../detections/candidate_detector2/comp4_det_test_%s.txt';
-    resultdir = '../results/candidate_detector2';  
+    resultdir = '../results/candidate_detector2';
     detname = 'condidate_detector2';
   case 'candidate_detector3'
     detpath = '../detections/candidate_detector3/comp4_det_test_%s.txt';
-    resultdir = '../results/candidate_detector3';  
+    resultdir = '../results/candidate_detector3';
     detname = 'condidate_detector3';
   case 'candidate_detector4'
     detpath = '../detections/candidate_detector4/comp4_det_test_%s.txt';
-    resultdir = '../results/candidate_detector4';  
+    resultdir = '../results/candidate_detector4';
     detname = 'condidate_detector4';
   case 'candidate_detector5'
     detpath = '../detections/candidate_detector5/comp4_det_test_%s.txt';
-    resultdir = '../results/candidate_detector5';  
+    resultdir = '../results/candidate_detector5';
     detname = 'candidate_detector5';
   otherwise
     error('unknown detector')
